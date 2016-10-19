209e877b22104149479e2f5d1ce54531df063d4c
Andrei Cornea <andrei.cornea@ad.fortech.ro>
2016-10-19 17:49:17 +0300

    display default api cal

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+
+(function(){
+    var app = angular.module('carStore',[]);
+    
+    app.controller('StoreController',["$http","$scope",function($http,$scope){
+
+        $http.get('http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=e6jd7d4rx7qx64r5dskzwdwc').success(function(response){ //make a get request to mock json file.
+            $scope.cars = response; //Assign data received to $scope.data
+              console.log(response);
+        })
+        .error(function(err){
+            //handle error
+            console.log("Error:",err);
+        });
+            }] );
+
+    app.controller("PanelController", function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+    
+
+    // var gem=[
+    // {
+    //     name:"Dodecahedron",
+    //     price:110.50,
+    //     description:"some gem have hidden qualities beyond",
+    //     canPurchase:true,
+    //     soldOut:false
+    // },
+    // {
+    //     name:"Dodecahedron",
+    //     price:110.50,
+    //     description:"some gem have hidden qualities beyond",
+    //     canPurchase:true,
+    //     soldOut:false
+    // }]
+})()
