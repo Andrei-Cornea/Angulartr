0bf4be701adf8e5559949202c0af9e250710f679
Andrei Cornea <andrei.cornea@ad.fortech.ro>
2016-10-24 18:00:18 +0300

    filter update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,25 +3,40 @@
     var app = angular.module('carStore',[]);
     
     app.controller('StoreController',["$http","$scope",function($http,$scope){
-       
+        $scope.cars=[];
+        $scope.cars.makes=[];
+        //  $scope.filter.make=[];
+        $scope.filter = {
+            make:"",
+            model:""
+        }
+        
+    console.log($scope.filter);
+
         $http({
             method: 'GET',
             url: 'http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=e6jd7d4rx7qx64r5dskzwdwc'
-            }).then(function successCallback(response) {
-                // this callback will be called asynchronously
-                // when the response is available
-                $scope.cars=response.data;
-                // console.log($scope.cars);
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                console.log("No response from the server!")
-            });
-              
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.cars = response.data;
+            // console.log($scope.cars);
+            // $scope.models = $scope.cars.makes.filter(function(car,filter){
+            //         if(car.name == "Acura"){
+            //             console.log(car.models);
+            //         //    return car.models;
+            //         }
+            //     });
 
-                console.log( $scope)
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log("No response from the server!")
+        });
 
-            }] );
+
+     }] );
+
 
     app.controller("PanelController", function(){
         this.tab = 1;
