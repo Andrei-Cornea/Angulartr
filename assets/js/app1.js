bee8a85b58437aee2eb550bd9636deb15d894589
Andrei Cornea <andrei.cornea@ad.fortech.ro>
2016-10-26 17:55:10 +0300

    filter done, addto cart method + ui update

--- assets/js/app1.js
+++ assets/js/app1.js
@@ -3,29 +3,32 @@
 
   app.controller("StoreController",["$http","dataService",function($http, dataService){
     var vm = this;
-    vm.cars =[];
-    vm.filter={
-      search:'',
-      make:'',
-      model:''
-    }
-    vm.models=[];
+    vm.cart=[];
 
     $http.get("http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=e6jd7d4rx7qx64r5dskzwdwc")
           .success(function(data){
         // vm.cars = data;
         vm.cars = dataService.processingData(data);
 
-        // console.log(data);
+        console.log(vm.cars);
     })
 
-  // vm.getmodels= function(make){
-  //   vm.cars.makes.filter(function(car){
-  //     if(car.name === make){
-  //       vm.models = car.models;
-  //     }
-  //   })
-  // }
+    vm.addToCart = function(val){
+        var add = true;
+
+        vm.cart.forEach(function(item){
+              if(item.model === val.model ){
+                  add=false;
+              }
+        })
+
+        if(add){
+           vm.cart.push(val);
+           console.log("added");
+        }
+        console.log(vm.cart);
+    }
+
 
   }])
 
@@ -44,24 +47,57 @@
   app.factory("dataService",function(){
     return{
       processingData: function(input){
-        var cars =[];
-        var result = input.makes.forEach(function(entry){
-            var item={};
-            item.name = entry.name;
-            item.models =[];
+        var data = {};
+        data.models=[];
+        data.name=[];
 
-             entry.models.forEach(function(model){
-              item.models.push(model.name);
-            });
-            cars.push(item);
+        input.makes.forEach(function(entry){
+
+          data.name.push(entry.name);
+          entry.models.forEach(function(model){
+            data.models.push({name:model.name,make:entry.name});
+          });
+
         })
-        console.log(cars);
+        // console.log(data);
 
-          return cars;
+        return data;
       }
     }
   })
 
 
-
 })()
+
+
+// app.factory("dataService",function(){
+//   return{
+//     processingData: function(){
+//       var data = {};
+//       data.models=[];
+//       data.name=[];
+//
+//       input.makes.forEach(function(entry){
+//
+//         data.name.push(entry.name);
+//         entry.models.forEach(function(model){
+//           data.models.push({name:model.name,make:entry.name});
+//         });
+//
+//       })
+//       // console.log(data);
+//
+//       return data;
+//     }
+//   }
+// })
+
+
+
+// vm.getmodels= function(make){
+//   vm.cars.makes.filter(function(car){
+//     if(car.name === make){
+//       vm.models = car.models;
+//     }
+//   })
+// }
