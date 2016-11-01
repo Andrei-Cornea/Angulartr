4fabb8870713475896661e4e815403a718081a88
ghele <gheletus_raoul@yahoo.com>
2016-11-01 15:32:47 +0200

    store and clear data try1

--- assets/js/app1.js
+++ assets/js/app1.js
@@ -1,62 +1,53 @@
-(function(){
-  var app = angular.module("carStore",['ui.bootstrap']);
+// (function(){
+  // var app = angular.module("carStore",['ui.bootstrap']);
 
   app.controller("StoreController",["$http","dataService","$uibModal","$log","$document",function($http, dataService, $uibModal,  $log, $document){
     var vm = this;
-    vm.cart=[];
-    vm.itemsInCart = null ;
+    vm.cart = [];
+    vm.itemsInCart = null;
 
     dataService.getData().then(function(data){
       vm.cars = data;
-        console.log(vm.cars );
+        console.log(vm.cars);
     })
 
     vm.addToCart = function(val){
         var add = true;
-
         vm.cart.forEach(function(item){
-              if(item.model === val.model ){
+              if(item.model === val.model) {
                   add=false;
               }
         })
-
-        vm.cars.models.forEach(function(item){
-              if(item.id === val.id){
+        vm.cars.models.forEach(function(item) {
+              if(item.id === val.id) {
                 item.selected = true;
               }
         })
 
-
-        if(add){
-
+        if(add) {
            vm.cart.push(val);
            vm.itemsInCart = vm.cart.length;
            console.log("added");
         }
-        console.log("Cart -",vm.cart);
-        console.log("Cars -",vm.cars);
+        console.log("Cart -", vm.cart);
+        console.log("Cars -", vm.cars);
     }
 
-    vm.removeCartItem= function(item){
+    vm.removeCartItem = function(item) {
       console.log(item.id);
-
-      vm.cars.models.forEach(function(model){
-
+      vm.cars.models.forEach(function(model) {
             if(model.id === item.id){
                model.selected = false;
             }
       })
-
       vm.cart.splice(item,1);
       vm.itemsInCart = vm.cart.length;
       console.log("removed!");
       console.log("Cart -",vm.cart);
       console.log("Cars -",vm.cars);
-
     }
 
-
-    vm.openModal = function(size){
+    vm.openModal = function(size) {
       var modalInstance = $uibModal.open({
         animation: "true",
         ariaLabelledBy: 'modal-title',
@@ -75,12 +66,11 @@
               }
       })
     }
-
   }])
 
   app.controller("PanelController",function(){
     var vm = this;
-    vm.tab=1;
+    vm.tab = 1;
 
     vm.selectTab = function(val){
       vm.tab= val;
@@ -90,71 +80,53 @@
     }
   })
 
-
-  app.factory("dataService",['$http',function($http){
-    var data = {};
-    data.models=[];
-    data.name=[];
-
-    var getData = function(){
-         return  $http.get("http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=e6jd7d4rx7qx64r5dskzwdwc")
-             .then(function(response){
-                    var id = 0;
-
-                     response.data.makes.forEach(function(entry){
-                       data.name.push(entry.name);
-                         entry.models.forEach(function(model){
-                           data.models.push(
-                             {
-                             name:model.name,
-                             make:entry.name,
-                             id:id++,
-                             selected:false
-                           });
-
-                         });
-
-                     })
-
-                     console.log(data);
-                     return data;
-                   })
-           }
-
-        return{
-          getData: getData
-        }
-
-  }])
-
-
-
 angular.module('carStore')
-       .controller('ModalInstanceCtrl', function ($uibModalInstance, cart) {
+       .controller('ModalInstanceCtrl', ['$window', '$uibModalInstance', 'cart', 'itemsInCart', function ($window, $uibModalInstance, cart, itemsInCart) {
 
   var vm = this;
   vm.cart = cart;
-  vm.itemsInCart = vm.cart.length;
+  vm.itemsInCart = itemsInCart;
 
-  vm.ok = function () {
+  vm.orders = [];
+
+    vm.orderId = 0;
+    vm.storeOrder = function () {
+    console.log(vm.cart);
+    console.log(vm.user);
+
+    vm.finalOrder = {
+      orderId: 'orderNo-' + vm.orderId,
+      customerName: vm.user.name,
+      customerEmail: vm.user.mail,
+      customerReview: vm.user.review,
+      orderDescription: vm.cart
+    }
+    vm.orders.push(vm.finalOrder);
+    vm.orderId++;
+
+    console.log(vm.finalOrder)
+    console.log("wedgwregweg " +vm.orders)
+
+vm.cart.length = 0;
+  vm.itemsInCart = vm.cart.length;
+console.log(vm.cart)
     $uibModalInstance.close();
+
   };
 
   vm.validateName = function(val){
-
     var patt = /^(?!.*([A-Za-z0-9])\1{1})[A-Za-z0-9]{5,}$/;
-
     return patt.test(val);
   }
 
   vm.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
-});
+}]);
 
 
 
-})()
+// })()
 // app.factory("dataService",function(){
 //   return{
 //     processingData: function(){
---
+++ assets/js/carFactory.js
@@ -0,0 +1,38 @@
+
+var app = angular.module("carStore",['ui.bootstrap']);
+
+app.factory("dataService",['$http',function($http){
+  var data = {};
+  data.models=[];
+  data.name=[];
+
+  var getData = function(){
+       return  $http.get("http://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=e6jd7d4rx7qx64r5dskzwdwc")
+           .then(function(response){
+                  var id = 0;
+
+                   response.data.makes.forEach(function(entry){
+                     data.name.push(entry.name);
+                       entry.models.forEach(function(model){
+                         data.models.push(
+                           {
+                           name:model.name,
+                           make:entry.name,
+                           id:id++,
+                           selected:false
+                         });
+
+                       });
+
+                   })
+
+                   console.log(data);
+                   return data;
+                 })
+         }
+
+      return{
+        getData: getData
+      }
+
+}])
